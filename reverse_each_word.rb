9063dd710fc8ae3055ab2a5788a48fd934fdacd1
Andre Hochi <andrehochiwork@gmail.com>
2021-02-15 10:00:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(stringarg)
   stringarg1 = stringarg.split(" ")
   empty = []
   stringarg1.collect{
-    |str| empty.unshift(str)
+    |str| empty.push(str.reverse)
   }
   empty
 end
